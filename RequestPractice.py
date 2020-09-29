0ff967022de74ab14e9f437fb381d089c588e6d6
xdevore <54865093+xdevore@users.noreply.github.com>
2020-09-29 13:52:37 -0400

    Update RequestPractice.py

--- RequestPractice.py
+++ RequestPractice.py
@@ -1,1 +1,12 @@
+# importing requests library
+import requests 
 
+# using requests to recieve data about Nirvana from MusicBrainz
+response = requests.get('http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases',
+
+  # setting headers to accept JSON rather than XML
+  headers={'Accept' : 'application/json'}
+)
+
+# printing the JSON response
+print(response.json())
